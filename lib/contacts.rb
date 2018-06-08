5ef09e5597e7a1e6f9c210af46d293c3083938f8
Jed Peek <jedpeek@yahoo.com>
2018-06-08 19:46:50 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, data|
+    if person == "Freddy Mercury"
     data.each do |k, v|
       if v == :favorite_ice_cream_flavors
         v.each do |flavor|
